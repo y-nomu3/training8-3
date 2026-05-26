32a7e4092fea22fffd98364ee2c4c88d6d03f495
WIN-KLV03NP4M9L\traininguser <yusuke.nomura@skywill.jp>
2026-05-26 14:36:45 +0900

    useStateで入力値保持を追加

--- form0526/src/App.jsx
+++ form0526/src/App.jsx
@@ -1,20 +1,59 @@
+import { useState } from "react"
+
 function App() {
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    agree: false,
+  });
+
+  const handleChange = (event) => {
+    const { name, value, type, checked } = event.target;
+
+    setFormData({
+      ...formData,
+      [name]: type === "checkbox" ? checked : value,
+    });
+  };
+
   return (
     <div>
       <h1>フォーム管理</h1>
 
       <form>
         <label>名前</label>
-        <input type="text" />
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
 
         <label>メールアドレス</label>
-        <input type="email" />
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
 
         <label>パスワード</label>
-        <input type="password" />
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
 
         <label>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            name="agree"
+            checked={formData.agree}
+            onChange={handleChange}
+          />
           利用規約に同意する
         </label>
 
